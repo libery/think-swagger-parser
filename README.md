77c8e6faff6bfdbc9fc38759eb78e5a33c3a2242
liber <liber_2000@163.com>
2017-07-24 17:11:56 +0800

    init version

---
+++ README.md
@@ -0,0 +1,8 @@
+# think-swagger-parser
+
+[![Build Status](https://img.shields.io/travis/libery/think-swagger-parser/master.svg?style=flat-square)](https://travis-ci.org/libery/think-swagger-parser)
+[![Coverage Status](https://img.shields.io/coveralls/libery/think-swagger-parser/master.svg?style=flat-square)](https://coveralls.io/github/libery/think-swagger-parser?branch=master)
+[![npm](https://img.shields.io/npm/v/think-swagger-parser.svg?colorB=brightgreen&style=flat-square)](https://www.npmjs.com/package/think-swagger-parser)
+[![devDependency Status](https://david-dm.org/libery/think-swagger-parser.svg)](https://david-dm.org/libery/think-swagger-parser)
+
+Parse Swagger doc for ThinkJS 3.x
---
+++ index.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const helper = require('think-helper');
+const SwaggerParser = require('./lib/think-swagger-parser');
+
+
+
+think.app.once('appReady', () => {
+
+});
+
+function invokeParser(options, app) {
+  app.think.beforeStartServer(async() => {
+    var apiDoc = think.config('api_doc');
+    var controllerDir = think.config('controller_dir');
+    var handler = function (descs) {
+      think.app.apiDescs = descs;
+      // console.log('parser think.app.apiDescs=' + JSON.stringify(parser.getDescs()));
+    };
+    var opt = {
+      // API文档路径
+      doc: apiDoc || './api/example.yaml',
+      // controllers的目录
+      controller_dir: controllerDir || './app/controller',
+      // 对接口做版本控制
+      versioning: true,
+      // 展示api-explorer
+      apiExplorerVisible: true,
+      handler: handler
+    };
+    // console.log('create SwaggerParser');
+    var parser = new SwaggerParser(opt);
+    return parser.load(handler);
+    // parser.load().then(()=>{
+  });
+  //   think.app.apiDescs = parser.getDescs();
+  return (ctx, next) => {
+    return next();
+  };
+  // });
+
+};
+
+module.exports = invokeParser;
